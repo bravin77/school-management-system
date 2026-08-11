7dae5dc1ba3e9e4cd9c355a70356568daa7f451f
Bravin Makanga <bravinmakanga@gmail.com>
2026-08-11 13:32:09 +0300

    CORS Configuration

--- core/settings.py
+++ core/settings.py
@@ -285,7 +285,7 @@ CORS_ALLOWED_ORIGINS = [
     "http://127.0.0.1:3000",
     "https://school-management-system-e8uewvses-develop-a148.vercel.app",
 ]
-
+print("CORS ALLOWED ORIGINS:", CORS_ALLOWED_ORIGINS)
 
 # ============================================================
 # VERCEL FRONTEND

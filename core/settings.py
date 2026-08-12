f643f96ec67de6c56bbcc1c22d5d97ac6a16b601
Bravin Makanga <bravinmakanga@gmail.com>
2026-08-12 09:36:18 +0300

    tweaked database on the settings file

--- core/settings.py
+++ core/settings.py
@@ -147,15 +147,11 @@ DATABASE_URL = os.environ.get(
 if DATABASE_URL:
 
     DATABASES = {
-
-        "default":
-            dj_database_url.parse(
-                DATABASE_URL,
-                conn_max_age=600,
-                ssl_require=True,
-            )
-
+    "default": {
+        "ENGINE": "django.db.backends.sqlite3",
+        "NAME": BASE_DIR / "db.sqlite3",
     }
+}
 
 else:
 
